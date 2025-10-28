b25e16f1426bbaf10e66a1959bac4c2bc764a62f
dariusp0p <dariuspop2005@gmail.com>
2025-10-28 15:16:28 +0200

    lab2: updated 3_server

--- lab2/3_server.c
+++ lab2/3_server.c
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
 #include <math.h>
@@ -38,24 +39,58 @@ client_info clients[MAX_CLIENTS];
 int client_count = 0;
 float srf;
 
+pthread_mutex_t timer_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t timer_cond = PTHREAD_COND_INITIALIZER;
+int timeout = 0;
 
-void* handle_client(void* arg) {
+
+void* timer_thread_func(void* arg) {
+    while (1) {
+        pthread_mutex_lock(&timer_mutex);
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_sec += 10;
+        int rc = pthread_cond_timedwait(&timer_cond, &timer_mutex, &ts);
+        if (rc == ETIMEDOUT) {
+            timeout = 1;
+            pthread_mutex_unlock(&timer_mutex);
+            break;
+        }
+        pthread_mutex_unlock(&timer_mutex);
+    }
     return NULL;
 }
+
+void* handle_client(void* arg) {
+    client_info* info = arg;
+    float crf;
+    int n = read(info->conn, &crf, sizeof(float));
+    if (n == sizeof(float)) {
+        info->crf = crf;
+    }
+    printf("Player %d guess is: %f\n", info->id, crf);
+    return NULL;
+}
+
 
 int main() {
     struct sockaddr_in client, server;
     int sock, code, len, conn;
     pthread_t tids[MAX_CLIENTS];
+    pthread_t timer_thread;
 
     srand(time(NULL));
     srf = (float)rand() / (float)RAND_MAX;
+    printf("The number is %f\n", srf);
 
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Error creating server socket!");
         return 1;
     }
+
+    int flags = fcntl(sock, F_GETFL, 0);
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 
     memset(&server, 0, sizeof(server));
     server.sin_port = htons(4321);
@@ -70,33 +105,35 @@ int main() {
 
     listen(sock, 10);
 
-    time_t last_conn = time(NULL);
+    pthread_create(&timer_thread, NULL, timer_thread_func, NULL);
 
-    while (1) {
+    while (!timeout) {
         memset(&client, 0, sizeof(client));
         len = sizeof(client);
 
         printf("Listening for incomming connections...\n");
         conn = accept(sock, (struct sockaddr *)&client, &len);
         if (conn < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                usleep(1000000);
+                continue;
+            }
             perror("Error accept!");
             continue;
         }
-        printf("Connection income: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        printf("New player from: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
-        last_conn = time(NULL);
+        pthread_mutex_lock(&timer_mutex);
+        pthread_cond_signal(&timer_cond);
+        pthread_mutex_unlock(&timer_mutex);
 
         clients[client_count].conn = conn;
         clients[client_count].id = client_count;
         pthread_create(&tids[client_count], NULL, handle_client, &clients[client_count]);
         client_count++;
-
-        if (client_count > 0 && time(NULL) - last_conn > 10) {
-            printf("Timeout!");
-            break;
-        }
     }
 
+    pthread_join(timer_thread, NULL);
     for (int i = 0; i < client_count; i++)
         pthread_join(tids[i], NULL);
 
@@ -113,10 +150,12 @@ int main() {
     for (int i = 0; i < client_count; i++) {
         if (i == winner) {
             char msg[128];
-            snprintf(msg, sizeof(msg), "You have the best guess with an error of %f\n", min_err);
+            snprintf(msg, sizeof(msg), "You won! The number was %f\n", srf);
             write(clients[i].conn, msg, strlen(msg));
         } else {
-            write(clients[i].conn, "You lost !\n", 10);
+            char msg[128];
+            snprintf(msg, sizeof(msg), "You lost! The number was %f\n", srf);
+            write(clients[i].conn, msg, strlen(msg));
         }
         close(clients[i].conn);
     }
